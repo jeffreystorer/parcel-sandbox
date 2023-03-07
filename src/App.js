4fa6c59d76eb33be4c132e293b68163fd8329b50
jeffreystorer <jeffrey.storer@gmail.com>
2023-03-07 12:33:59 -0500

    3/7: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/index.css';
+
+export default function App() {
+  return (
+    <>
+      <div className='center'>
+        <h1>Testing Parcel</h1>
+        <h2>Header 2</h2>
+        <p>Some Text Here</p>
+        <p>More Text Here</p>
+      </div>
+    </>
+  );
+}
