0043d34d0f403b2a81d28ea58ef89b9f337b3b52
jeffreystorer <jeffrey.storer@gmail.com>
2023-03-07 13:26:39 -0500

    3/7: Add Packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { get, set } from '/packages/localStorage';
+import capitalize from '/packages/capitalize';
 import './styles/index.css';
 
 export default function App() {
+  set('date', new Date());
+  const setDate = get('date');
+  const lowerName = 'jeff';
+  const capName = capitalize(lowerName);
   return (
     <>
       <div className='center'>
-        <h1>Testing Parcel</h1>
+        <h1>
+          {capName} is Testing Parcel on {setDate}
+        </h1>
         <h2>Header 2</h2>
         <p>Some Text Here</p>
         <p>More Text Here</p>
