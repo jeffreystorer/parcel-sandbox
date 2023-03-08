3a6684940425476523f1a70bae6812177d208bb7
jeffreystorer <jeffrey.storer@gmail.com>
2023-03-07 23:24:32 -0500

    3/7: Put Styles Inline

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import { get, set } from '/packages/localStorage';
 import capitalize from '/packages/capitalize';
-import './styles/index.css';
 
 export default function App() {
   set('date', new Date());
   const setDate = get('date');
   const lowerName = 'jeff';
   const capName = capitalize(lowerName);
+
+  const centerStyle = {
+    textAlign = "center"
+  }
+
+  const h1Style = {
+    color: 'green',
+    fontFamily: 'cursive',
+  };
+
+  const h2Style = {
+    color: 'red',
+  };
+
+  const pStyle = {
+    color: 'purple',
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    fontWeight: 'bold',
+    fontSize: '1rem',
+  };
   return (
     <>
-      <div className='center'>
-        <h1>
+      <div style={centerStyle}>
+        <h1 style={h1Style}>
           {capName} is Testing Parcel on {setDate}
         </h1>
-        <h2>Header 2</h2>
-        <p>Some Text Here</p>
-        <p>More Text Here</p>
+        <h2 style={h2Style}>Header 2</h2>
+        <p style={pStyle}>Some Text Here</p>
+        <p style={pStyle}>More Text Here</p>
       </div>
     </>
   );
